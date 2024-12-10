0c988f0beb140699f1283495e5a70e8315ef23c6
cedricho <12634887+cedricho@users.noreply.github.com>
2024-12-10 14:56:17 -0800

    use numpy array

--- aoc/2024/day10.py
+++ aoc/2024/day10.py
@@ -3,10 +3,7 @@ import numpy as np
 import sys
 
 
-width = height = 0
-
-
-def in_range(x, y):
+def in_range(x, y, height, width):
   return 0 <= x < width and 0 <= y < height
 
 
@@ -19,15 +16,14 @@ def calc_path(grid, sx, sy, allowDup = False):
     if grid[y, x] == 9: peak.append((x, y)); continue
     candidates = [(x + dx, y + dy) for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]]
     for cx, cy in candidates:
-      if in_range(cx, cy) and grid[cy, cx] == grid[y, x] + 1:
+      if in_range(cx, cy, *grid.shape) and grid[cy, cx] == grid[y, x] + 1:
         q.append((cx, cy))
   return len(peak) if allowDup else len(set(peak))
 
 
 with open(sys.argv[1]) as input:
   grid  = np.genfromtxt(input, delimiter=1)
-  width, height = grid.shape
-  paths = sum([calc_path(grid, x, y) for y in range(height) for x in range(width) if grid[y][x] == 0])
+  paths = sum([calc_path(grid, x, y) for (y, x), v in np.ndenumerate(grid) if v == 0])
   print("Part 1", paths)
-  paths = sum([calc_path(grid, x, y, allowDup=True) for y in range(height) for x in range(width) if grid[y][x] == 0])
+  paths = sum([calc_path(grid, x, y, allowDup=True) for (y, x), v in np.ndenumerate(grid) if v == 0])
   print("Part 2", paths)
